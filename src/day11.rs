d762dd8a97e044262319fb20aa7fe95cf9a7b024
Will Barnett <william@barnett.au>
2024-12-11 19:43:18 +1100

    use or_default

--- src/day11.rs
+++ src/day11.rs
@@ -3,18 +3,18 @@ use std::collections::HashMap;
 fn solve(input: String, blinks: u64) -> String {
   let mut rocks: HashMap<u64, u64> = HashMap::new();
   for n in input.split_whitespace() {
-    *rocks.entry(n.parse().unwrap()).or_insert(0) += 1;
+    *rocks.entry(n.parse().unwrap()).or_default() += 1;
   }
   for _ in 0 .. blinks {
     let mut new_rocks = HashMap::new();
     for (rock, count) in rocks {
       let name = rock.to_string();
       if rock == 0 {
-        *new_rocks.entry(1).or_insert(0) += count;
+        *new_rocks.entry(1).or_default() += count;
       } else if name.len() % 2 == 0 {
         let (left, right) = name.split_at(name.len() / 2);
-        *new_rocks.entry(left.parse().unwrap()).or_insert(0) += count;
-        *new_rocks.entry(right.parse().unwrap()).or_insert(0) += count;
+        *new_rocks.entry(left.parse().unwrap()).or_default() += count;
+        *new_rocks.entry(right.parse().unwrap()).or_default() += count;
       } else {
         *new_rocks.entry(rock * 2024).or_insert(0) += count;
       }
